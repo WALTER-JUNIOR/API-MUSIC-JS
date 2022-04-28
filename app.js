bc197aded5e4920edc523aab44c490beb3715894
unknown <walvaradocruz@gmail.com>
2022-04-28 13:13:43 -0500

    API-MUSIC

---
+++ app.js
@@ -0,0 +1,58 @@
+const PrintCard = document.querySelector(".PrintCard");
+const formulario = document.querySelector("#formulario");
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetchData();
+});
+
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(formulario);
+  const [cancion] = [...formData.values()];
+
+  fetchData(cancion);
+  formulario.reset();
+});
+
+const fetchData = async (cancion) => {
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+      "X-RapidAPI-Key": "ff481a78a2msh4196a63b4cb27c4p15dae2jsn72d9cdff93b9",
+    },
+  };
+
+  try {
+    const MusicData = await fetch(
+      `https://deezerdevs-deezer.p.rapidapi.com/search?q=${cancion}`,
+      options
+    );
+    const res = await MusicData.json();
+
+    let cancionesArr = res.data;
+    cancionesCard(cancionesArr);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const cancionesCard = (cancionesArr) => {
+  PrintCard.textContent = "";
+  //   console.log(cancionesArr);
+  cancionesArr.forEach((item) => {
+    // console.log(item.title);
+    // console.log(PrintCard)
+    PrintCard.innerHTML += `<div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem">
+                                    <img src="${item.album.cover_medium}" class="card-img-top" alt="" />
+                                    <div class="card-body">
+                                        <h5 class="card-title">${item.title}</h5>
+                                        <audio class="w-100" controls name="media">
+                                            <source src="${item.preview}" type="audio/mpeg">
+                                        </audio>
+                                        <a href="${item.link}" target="_blank" class="btn btn-primary mt-2">Go somewhere</a>
+                                    </div>
+                                </div>`;
+  });
+};
